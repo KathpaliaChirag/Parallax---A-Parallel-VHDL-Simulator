617afa7d0fe97224b103e81b4ca5b27dd7d02d4d
Chirag Kathpalia <chiragkathpalia1@gmail.com>
2026-02-26 14:25:57 +0530

    just added an event struct with headerguards

---
+++ event.h
@@ -0,0 +1,17 @@
+//ifndef is called a header guard
+/*
+why its needed lets say 2 files have event.h included in them what would happen
+compiler may see them and get confused and may throw a redefinition error
+ifndef = if not defined yet
+then okay define now (2nd line)
+*/
+#ifndef EVENT_H
+#define EVENT_H
+
+typedef struct 
+    {
+        double time;
+        int type;
+    } Event;
+
+#endif
