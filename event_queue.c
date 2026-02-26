9299501721d9c6645f73c10b1e7337919b40f503
Chirag Kathpalia <chiragkathpalia1@gmail.com>
2026-02-26 16:27:44 +0530

    just made a function to extract min...seems its working too

--- event_queue.c
+++ event_queue.c
@@ -20,10 +20,71 @@ EventQueue* init_queue()
 
     //interestingly this cast is about mapping...as in i could do a void ptr too but how would the copiler understand how much of memory to allocate ?
     //like if i do p->size =0... it wouldn't know...why or till where?
-    
+
     EventQueue *p1 = (EventQueue *)malloc(sizeof(EventQueue));
     p1->data= (Event *) malloc(sizeof(Event)*10);
     p1->size =0;
     p1->capacity =10;
     return p1;
+}
+
+//CHIRAG 26-02-26
+/*idea of insert ele goes like __this
+we first will be making a void function, taking 2 par queue and event e
+then we will add at the sie and increment size and then do heapify
+*/ 
+void insert_ele(EventQueue* p1, Event e)
+{
+    p1->data[p1->size]=e;
+    int tempsize = p1->size;
+    p1->size++;
+    //heapify starts here basically if its smaller than parent swap its place
+    while(p1->data[tempsize].time<p1->data[((tempsize)-1)/2].time)
+    {
+        Event temp = p1->data[tempsize];
+        p1->data[tempsize] = p1->data[(tempsize-1)/2];
+        p1->data[(tempsize-1)/2] = temp;
+        tempsize = (tempsize-1)/2;
+        if(tempsize ==0)
+        break;
+
+    }
+
+    return ;
+}
+
+//CHIRAG 26-02-26
+// next function i might need would be extract minimum
+// idea goes as follows ....
+// 1. extract root and save it,
+// 2. element at last index i will move to root
+// 3. then shrink size
+// 4. lastly i will simple heapify it down as long as it goes... maybe it might need more thought like what if both childern are same...maybe then we always pick left one
+
+Event extract_min(EventQueue *q)
+{
+    Event temp = q->data[0];
+    q->data[0]= q->data[q->size-1];
+    q->size--;
+    int i =0;
+    while(1)
+    {
+        if((2*i+1<q->size)&& q->data[i].time>q->data[2*i+1].time)
+        {
+            Event t= q->data[i];
+            q->data[i]= q->data[2*i+1];
+            q->data[2*i+1]= t;
+            i= 2*i+1;
+        }
+        else if((2*i+2<q->size) && q->data[i].time>q->data[2*i+2].time)
+        {
+            Event t= q->data[i];
+            q->data[i]= q->data[2*i+2];
+            q->data[2*i+2]= t;
+            i= 2*i+2;    
+        }
+        else
+        break;
+    }
+    return temp;
 }
