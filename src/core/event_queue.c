e24ed5a711a92c497be06b6208c8195a8112e02b
Chirag Kathpalia <chiragkathpalia1@gmail.com>
2026-03-09 19:31:29 +0530

    connected marcos implementation to eventqueue

--- src/core/event_queue.c
+++ src/core/event_queue.c
@@ -5,10 +5,17 @@
 // 1. it will be storing basic initialisation of queue init_queue
 // 2. it will also be responsible to inclrease capacity when its close to full
 // 3. maybe add and remove events
+//CHIRAG 09-03-26 
+// so well i have decided to change the static implemenatation of event queue to marcos type DYNARRAY so everywhere EventQueue becomes DynArray_event
+// \ or i can do a simple rename well thats better actually will do change name in .h file
+
 #include "event_queue.h"
 #include <stdio.h>
 #include<stdlib.h> //used for malloc
-EventQueue* init_queue()
+#include "utils.h"
+// CHIRAG : 09-03-26 18:57 --------------
+//idea is to relace this whole with new marcos implementation
+EventQueue init_queue()
 {
     // just had some doubt about syntax so took help of internet
     // was just thinking whats need of (EventQueue *)before malloc
@@ -21,12 +28,18 @@ EventQueue* init_queue()
     //interestingly this cast is about mapping...as in i could do a void ptr too but how would the copiler understand how much of memory to allocate ?
     //like if i do p->size =0... it wouldn't know...why or till where?
 
-    EventQueue *p1 = (EventQueue *)malloc(sizeof(EventQueue));
-    p1->data= (Event *) malloc(sizeof(Event)*10);
-    p1->size =0;
-    p1->capacity =10;
+    // EventQueue *p1 = (EventQueue *)malloc(sizeof(EventQueue));
+    // p1->data= (Event *) malloc(sizeof(Event)*10);
+    // p1->size =0;
+    // p1->capacity =10;
+
+    //in new implementation marco simply replaces a part of code 
+    EventQueue p1;
+    DYNARRAY_INIT(p1)
     return p1;
 }
+//---------------------------------------------------------------
+
 
 //CHIRAG 26-02-26
 /*idea of insert ele goes like __this
@@ -35,9 +48,11 @@ then we will add at the sie and increment size and then do heapify
 */ 
 void insert_ele(EventQueue* p1, Event e)
 {
-    p1->data[p1->size]=e;
-    int tempsize = p1->size;
-    p1->size++;
+    //CHIRAG 09-03-26 19:20 : Updated it and added marcos call
+    DYNARRAY_INSERT((*p1), e)
+    int tempsize = p1->size-1;
+    // p1->data[p1->size]=e;
+    // p1->size++;
     //heapify starts here basically if its smaller than parent swap its place
     while(p1->data[tempsize].time<p1->data[((tempsize)-1)/2].time)
     {
--- src/core/event_queue.h
+++ src/core/event_queue.h
@@ -4,11 +4,15 @@
 #ifndef EVENTQUEUE_H
 #define EVENTQUEUE_H
 #include "event.h"
+//CHIRAG -09-03-26 : replacing the manual type of struct event queue to an automated marco  
+#include "utils.h"
 
-typedef struct{
-    int size;
-    int capacity;
-    Event *data;
-} EventQueue;
+// typedef struct{
+//     int size;
+//     int capacity;
+//     Event *data;
+// } EventQueue;
 
+DYNARRAY_TYPE(Event) //semicol not needed here as it litrally expands to ; in end 
+typedef DynArray_Event EventQueue; //this line helps in aliasing
 #endif
